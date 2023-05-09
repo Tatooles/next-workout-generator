0c442f27066d638972d2020649f8285c07c3fe5a
Kevin Tatooles <tatooles@wisc.edu>
2023-05-08 23:01:22 -0500

    Start connecting to OpenAI Ap

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -1,5 +1,21 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export async function POST(request: Request) {
-  const body = await request.json();
-  console.log(body);
-  return new Response("Ok");
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "What is the mascot of the Wisconsin Badgers?",
+      temperature: 0.6,
+    });
+    console.log(completion);
+    return new Response("Ok");
+  } catch (error) {
+    console.log("An error ocurred!");
+    if (error instanceof Error) console.log(error.message);
+  }
 }
