b375b75003396aae0295a8ff8649420796326e1c
Kevin Tatooles <tatooles@wisc.edu>
2023-05-12 22:29:58 -0500

    Generate workout based on all inputs

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -8,6 +8,7 @@ const openai = new OpenAIApi(configuration);
 export async function POST(request: Request) {
   const body = await request.json();
   const prompt = constructPrompt(body);
+  console.log(prompt);
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
@@ -29,12 +30,26 @@ export async function POST(request: Request) {
 }
 
 function constructPrompt(userInformation: any) {
+  console.log(userInformation);
   let prompt = "";
   if (userInformation.workoutType) {
-    prompt = `Generate a ${userInformation.workoutType} for me`;
+    prompt = `Generate a ${userInformation.workoutType} for me `;
+    if (userInformation.bodyParts.length) {
+      prompt += `with these additional body parts:`;
+      for (const part of userInformation.bodyParts) {
+        prompt += ` ${part}`;
+      }
+    }
+  } else {
+    if (userInformation.bodyParts.length) {
+      prompt = "Generate a workout for me to hit these body parts:";
+      for (const part of userInformation.bodyParts) {
+        prompt += ` ${part}`;
+      }
+    }
   }
   if (userInformation.additionalDetails) {
-    prompt += `Here are some additional details: ${userInformation.additionalDetails}`;
+    prompt += ` Here are some additional details: ${userInformation.additionalDetails}`;
   }
   return prompt;
 }
